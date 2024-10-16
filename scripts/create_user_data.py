7bbaea5c641e9d08b93b0960ace56e691a04ee77
Ngoc Quach <81842786+ngocxxu@users.noreply.github.com>
2024-10-16 14:28:00 +0700

    Update create_user_data.py

--- scripts/create_user_data.py
+++ scripts/create_user_data.py
@@ -1,23 +1,13 @@
-import pandas as pd
-from faker import Faker
+import pandas as pd # type: ignore
+from faker import Faker # type: ignore
 from datetime import datetime, timedelta
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-
-load_dotenv()
-
-sql_user_password = os.getenv('SQL_USER_PASSWORD')
-
-# Connect to CockroachDB
-jdbc_url = f"postgresql://ngocxxu:{sql_user_password}@bono-projects-3828.j77.aws-ap-southeast-1.cockroachlabs.cloud:26257/spark_cockroach_db?sslmode=verify-full"
 
 fake = Faker()
 
-# Create DataFrame to save data
+# Create a DataFrame to store user data
 user_data = pd.DataFrame(columns=['user_id', 'user_name', 'email', 'total_spend', 'date', 'age', 'gender', 'location'])
 
-# Create 100 samples
+# Create 100 sample users
 user_list = []
 start_date = datetime(2022, 1, 1)
 for i in range(100):
@@ -34,10 +24,5 @@ for i in range(100):
 
 user_data = pd.concat([user_data, pd.DataFrame(user_list)], ignore_index=True)
 
-# Save data into CockroachDB
-engine = create_engine(jdbc_url)
-
-# Record data into user_data table
-user_data.to_sql('user_data', con=engine, if_exists='replace', index=False)
-
-print("Data already saved in CockroachDB!")
+# Save file CSV
+user_data.to_csv('data/raw/user_data.csv', index=False)
